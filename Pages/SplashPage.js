aa68982dc46e83d53d0cf0f3a465c1feb7fcd29f
Mohammed Manssour <manssour.mohammed@gmail.com>
2018-09-20 01:55:07 +0300

    check for logged in status

--- Pages/SplashPage.js
+++ Pages/SplashPage.js
@@ -1,5 +1,6 @@
 const Api = require('Modules/Api');
 const Observable = require('FuseJS/Observable');
+const User = require('Modules/User');
 
 const state = Observable('loading');
 const message = Observable('Loading...');
@@ -9,19 +10,37 @@ Api.init()
     .json()
     .then(({content, status}) => {
         if(content.meta.code === 1){
-            state.value = 'success';
-            return;
+            return User.singleton()
+                .readFromStorage();
         }
 
         state.value = 'maintenance';
         message.value = 'Our server is in maintenance mode';
+    })
+    .then(response => {
+        if(response === 'noinfo'){
+            state.value = 'success';
+            return;
+        }
+
+        state.value = 'profile';
+        message.value = 'Loading Your profile....';
+
+        setTimeout(() => {
+            router.goto('loggedIn',{}, 'feed', {});
+        }, 2000)
     })
     .catch(e => {
         state.value = 'not-connected';
         message.value = "Please Check your internet connection";
     });
 
+function gotoRegister(){
+    router.push('register');
+}
+
 module.exports = {
     state,
-    message
+    message,
+    gotoRegister
 }
